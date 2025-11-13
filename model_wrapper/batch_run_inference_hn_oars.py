2371a71210966899f6b8cdfdcc872002a9627c66
aditiiyer <iyera@mskcc.org>
2025-11-13 10:45:25 -0500

    Single wrapper for batch processing

--- model_wrapper/batch_run_inference_hn_oars.py
+++ model_wrapper/batch_run_inference_hn_oars.py
@@ -1,6 +1,9 @@
+import sys
 import os
-import batch_run_inference_hn_oars_dcm
-import batch_run_inference_hn_oars_nii
+from pathlib import Path
+
+import run_inference_deeplab
+import run_inference_selfattn
 
 def main(inputPath, sessionPath, outputPath):
 
@@ -8,7 +11,7 @@ def main(inputPath, sessionPath, outputPath):
     os.makedirs(outputPath, exist_ok=True)
 
     # Examine input directory contents
-    contents = [os.path.join(path, f) for f in os.listdir(inputPath)]
+    contents = [os.path.join(inputPath, f) for f in os.listdir(inputPath)]
     if not contents:
         print("Input directory is empty.")
         return
@@ -19,9 +22,27 @@ def main(inputPath, sessionPath, outputPath):
 
     # Run batch auto-seg
     if dcmFlag:
-        batch_run_inference_hn_oars_dcm.main(inputPath, sessionPath, outputPath)
+        # Loop over pts
+        count = 0
+        for ptDir in contents:
+            count = count + 1
+            print('Segmenting dataset {} of {}'.format(count, len(contents)))
+            ptID = Path(Path(ptDir).stem).stem
+            ptSessionDir = os.path.join(sessionPath, ptID)
+            run_inference_deeplab.main(ptDir, ptSessionDir, outputPath)
+            run_inference_selfattn.main(ptDir, ptSessionDir, outputPath)
+
     elif niiFlag:
-        batch_run_inference_hn_oars_nii.main(inputPath, sessionPath, outputPath)
+        fileList = os.listdir(inputPath)
+        count = 0
+        for fileName in fileList:
+            count = count + 1
+            print('Segmenting file {} of {}'.format(count, len(fileList)))
+            ptID = os.path.basename(inputPath)
+            filePath = os.path.join(inputPath, fileName)
+            ptSessionDir = os.path.join(sessionPath, ptID)
+            run_inference_deeplab.main(filePath, ptSessionDir, outputPath)
+            run_inference_selfattn.main(filePath, ptSessionDir, outputPath)
 
     return 0
 
